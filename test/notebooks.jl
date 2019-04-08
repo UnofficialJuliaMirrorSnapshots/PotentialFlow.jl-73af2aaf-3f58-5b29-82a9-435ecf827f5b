545a598ca42527e55f6a63406f2d7ddd74098615
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-08 01:29:10 +0000

    Snapshot of branch master taken on 2019-04-07T21:29:06.564-04:00 from "https://github.com/UnofficialJuliaMirror/PotentialFlow.jl-73af2aaf-3f58-5b29-82a9-435ecf827f5b"

---
+++ test/notebooks.jl
@@ -0,0 +1,16 @@
+using NBInclude
+
+@testset "Jupyter notebooks" begin
+    notebooks = ["Pitching Plate - K07",
+                 "Point Source Demo",
+                 "Translating Plate - 20°",
+                 "Translating Plate - 60°",
+                 "Vortex Sheet Roll-up"]
+
+    notebook_dir = joinpath(splitdir(@__FILE__())[1], "../binder/notebooks")
+
+    @testset "$notebook" for notebook in notebooks
+        @nbinclude(joinpath(notebook_dir, "$notebook.ipynb"); softscope=true)
+        @test true
+    end
+end
